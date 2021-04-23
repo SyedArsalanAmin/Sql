45ef96c53fbd7299a4d35e8b582d0bd4b5d4cbdc
SyedArsalanAmin <sarsalanamin12@gmail.com>
2021-04-23 16:55:53 +0500

    some minor changes

--- sql_prac_v1.0.py
+++ sql_prac_v1.0.py
@@ -9,9 +9,38 @@ pd.read_sql("Select * from table1", conn)
 
 
 mycursor.execute(
-    "INSERT INTO table1 (id, first_name,	last_name,	salary) VALUES (100,'Syed Arsalan', 'Amin', 100)")
+    "INSERT INTO table1 (id, first_name,	last_name,	salary) VALUES (170,'sultan', 'tipu', 170)")
 conn.commit()
 
-# TODO: USing the execute statement
+# TODO: Using the Where statement
 
-mycursor.execute("select * from table1 where")
+mycursor.execute("select * from table1 where salary >= %s",
+                 (40,))  # thid %s will prevent sq injection
+result = mycursor.fetchall()
+
+for i in result:
+    print(i)
+
+
+# TODO: Using the count statement(tells the rows)
+
+mycursor.execute("select count(*) from table1")
+result = mycursor.fetchall()
+
+for i in result:
+    print(i)
+
+
+# TODO: Using order by, offset, limit:
+
+mycursor.execute("Select * from table1 order by salary Desc  limit 1 offset 0")
+result = mycursor.fetchall()
+for i in result:
+    print(i)
+
+# TODO: Using order by, offset, limit:
+
+mycursor.execute("Select * from table1")
+result = mycursor.fetchall()
+for i in result:
+    print(i)
